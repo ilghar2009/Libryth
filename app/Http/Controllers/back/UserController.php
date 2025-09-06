72705ee5545e974e9c41d19e3c24f8aba8c81a38
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-09-06 20:02:53 +0330

    create back/user_controller function's

--- app/Http/Controllers/back/UserController.php
+++ app/Http/Controllers/back/UserController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers\back;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $users = User::all();
+        return $users;
+    }
+
+    public function show(User $user){
+
+        $artist = $user->map(function ($item){
+            return
+                [
+                    'id' => $item->id,
+                    'title' => $item->title,
+                    'text' => $item->text,
+                    'music' => $item->music,
+                    'role'=> $item->role,
+                ];
+        });
+
+        return response()->json($artist);
+    }
+
+    public function update(User $user){
+        $user->update(['role' => !$user->role]);
+        return true;
+    }
 }
